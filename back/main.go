a14ac7064f16249098de32c3799a919e56514371
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-05 22:15:38 +0900

    main.go

--- back/main.go
+++ back/main.go
@@ -1,7 +1,17 @@
 package main
 
 import "fmt"
+//타입 지정
+type MessageStatus string
 
+const (
+	MESSAGE_FAIL        MessageStatus = "0"
+	MESSAGE_REQUEST     MessageStatus = "1"
+	MESSAGE_IN_PROGRESS MessageStatus = "2"
+	MESSAGE_SUCCESS     MessageStatus = "3"
+	MESSAGE_CANCELED    MessageStatus = "4"
+	MESSAGE_DUPLICATED  MessageStatus = "5"
+)
 func main() {
-fmt.Println("Hello, Go World!")
+fmt.Println(MESSAGE_FAIL)
 }
