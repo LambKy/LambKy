e9047ab0cae0705d2d51696bea45da15ae2595ce
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-07 16:55:05 +0900

    서버연결준비중

--- back/main.go
+++ back/main.go
@@ -1,17 +1,16 @@
 package main
 
 import "fmt"
-//타입 지정
-type MessageStatus string
 
-const (
-	MESSAGE_FAIL        MessageStatus = "0"
-	MESSAGE_REQUEST     MessageStatus = "1"
-	MESSAGE_IN_PROGRESS MessageStatus = "2"
-	MESSAGE_SUCCESS     MessageStatus = "3"
-	MESSAGE_CANCELED    MessageStatus = "4"
-	MESSAGE_DUPLICATED  MessageStatus = "5"
-)
-func main() {
-fmt.Println(MESSAGE_FAIL)
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge <18 {
+		return false
+	}
+	return true
+}
+func main(){
+
+
+	fmt.Println(canIDrink(16))
+
 }
