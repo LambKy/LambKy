36dd87da043352376730edaae6bfe611c58763a5
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-07 17:23:54 +0900

    데이터베이스 연결

--- back/main.go
+++ back/main.go
@@ -1,16 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge <18 {
-		return false
-	}
-	return true
+	"github.com/labstack/echo"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+//데이터베이스 지정
+type User struct {
+	Name string `json:"name" xml:"name" form:"name" query:"name"`
+	Idx  string `json:"idx" xml:"idx" form:"idx" query:"idx"`
+	Pw   string `json:"pw" xml:"pw" form:"pw" query:"pw"`
 }
-func main(){
+//데이터베이스 지정
+func (User) TableName() string {
+	return "user"
+}
+//서버연결
+//dsn := "이름:비밀번호@tcp(Host)/데이터베이스?charset=utf8mb4&parseTime=True&loc=Local"
+func main() {
+	dsn := "hyun:12341234@tcp(127.0.0.1)/const2?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic("Db연결에 실패")
+	}
 
+	db.AutoMigrate(&User{})
 
-	fmt.Println(canIDrink(16))
+	e := echo.New()
+	e.Static("/static", "page")
 
+	e.File("/join", "page/join.htm")
+	e.File("/complate", "page/complate.htm")
+
+	//가입
+	e.POST("/join", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		if len(u.Name) == 0 {
+			return c.HTML(http.StatusOK, "<script>window.location.href='http://localhost:1323/join';alert('네임을 입력하세요.')</script>")
+		}
+		if len(u.Idx) < 5 {
+			return c.HTML(http.StatusOK, "<script>window.location.href='http://localhost:1323/join';alert('아이디를 입력하세요.')</script>")
+		}
+		if len(u.Pw) < 8 {
+			return c.HTML(http.StatusOK, "<script>window.location.href='http://localhost:1323/join';alert('패스워드를 입력하세요.')</script>")
+		}
+		ur := &User{
+			Idx:  u.Idx,
+			Pw:   u.Pw,
+			Name: u.Name,
+		}
+		db.Create(&ur)
+		return c.HTML(http.StatusOK, "<script>window.location.href='http://localhost:1323/complate';</script>")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
